e4eb9259d4cc65d15ab93ca9e43690320fbe8512
Anton <frostanton09@gmail.com>
2019-10-04 16:39:40 +0300

    Many changes in back and front end

---
+++ backend/src/main/java/com/project/logistics/controller/RoleController.java
@@ -0,0 +1,23 @@
+package com.project.logistics.controller;
+
+import com.project.logistics.entity.RoleEntity;
+import com.project.logistics.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/role")
+public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+
+    @GetMapping("/all")
+    public List<RoleEntity> getAllRoles() {
+        return roleService.getAllRoles();
+    }
+}
---
+++ backend/src/main/java/com/project/logistics/service/impl/RoleServiceImpl.java
@@ -0,0 +1,21 @@
+package com.project.logistics.service.impl;
+
+import com.project.logistics.entity.RoleEntity;
+import com.project.logistics.repository.RoleRepository;
+import com.project.logistics.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public List<RoleEntity> getAllRoles() {
+        return roleRepository.getAll();
+    }
+}
